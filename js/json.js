c820afa4ceeae9cf0e44366c0739fb1edc941e55
MaxKicw <maximilian.wick@student.hs-rm.de>
2018-01-02 21:20:06 +0100

    JSON-Tweck 1
    .stringify wird getestet da ein JS Objekt anscheinend in einem JS-Objekt nicht funktioniert :anguished:

--- js/json.js
+++ js/json.js
@@ -1,12 +1,11 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET',"https://raw.githubusercontent.com/MaxKicw/jsontest/master/script.json");
+ourRequest.open('GET',"https://raw.githubusercontent.com/MaxKicw/65kmh/master/js/data.json");
 var jsonData;
 ourRequest.onload = function(){
     jsonData = JSON.parse(ourRequest.responseText);
     console.log(jsonData);
 };
-var ourData = JSON.parse(jsonData);
-console.log(ourData);
+
 ourRequest.send();
 
            

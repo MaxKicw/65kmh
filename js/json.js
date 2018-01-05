51ef6d9ef038a21b7f344a20cc946db383f6118d
MaxKicw <maximilian.wick@student.hs-rm.de>
2018-01-05 12:12:24 +0100

    JSON - Stage 2 :two:
    Die Marker werden durch den Inhalt des JSON-Responses gesetzt und Karten werden automatisch in das richtige Feld gesetzt und befüllt. Next on the list ist:
    - Wenn über eine Karte hovern soll oben der Marker angezeigt werden
    - Karte soll desiged werden und mit dynamischen Inhalt befüllt werden :100:

--- js/json.js
+++
@@ -1,9 +0,0 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET',"https://raw.githubusercontent.com/MaxKicw/65kmh/master/js/data.json");
-var ourData;
-ourRequest.onload = function(){
-    ourData = JSON.parse(ourRequest.responseText);
-};
-ourRequest.send();
-
-           
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*------GoogleMapsApi-----------------*/
+var ourData;
 function initMap() {
             //Optionen zur Anzeige der Karte//
             var options = {
@@ -7,34 +7,58 @@ function initMap() {
             }
             //Die Karte//
             var map = new google.maps.Map(document.getElementById('map'),options);
-            
             //Die Daten// 
-            var props;
-            for (var objekte in ourData){
-                props = ourData[objekte];
-                console.log(props);
-                ausflugMarker(props);
-            };
-            // Arbeitsfunktionen
-            function ausflugMarker(props){ 
-                    var marker = new google.maps.Marker({
-                        name:"ausflug"+props.number,
-                        position: new google.maps.LatLng(props.lat,props.lng),
-                        map:map,
-                        icon:"",
-                    });
-                    var infoFenster = new google.maps.InfoWindow({
-                        content:'<div class="pop"><p>'+props.info+'</p></div>'
-                    }); 
-                    
-                    marker.addListener('click',function(){
-                        infoFenster.open(map,marker);
-                    })
-                    
-                    if(props.status === "F"){
-                        marker.icon = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-                    }
-                    console.log(marker)
+            //--------XMLRequest------------------//
+            //Besorgt die JSON-Daten über einen XML-Request an den gespeicherten Pfad//
+            var ourRequest = new XMLHttpRequest();
+            ourRequest.open('GET',"https://raw.githubusercontent.com/MaxKicw/65kmh/master/js/data.json");
+            ourRequest.send();
+            //--------XMLRequestEnde--------------//
+            //--------Die Daten verarbeiten------//
+            ourRequest.onload = function(){
+                console.log("Our Data vor dem Request: "+ourData);
+                ourData = JSON.parse(ourRequest.responseText);
+                console.log("Our Data nach dem Request: "+ourData);
+            //Die einzenen Objekte werden einzeln aus dem Response, gespeichert in ourData itteriert und die ausflugMarker-Funktion mit dem Objekt ausgeführt.//
+                for (var objekte in ourData){
+                    props = ourData[objekte];
+                    console.log(props)
+                    ausflugMarker(props);
+                    renderHTML(props);
                 };
-}
-            
+                /*  Arbeitsfunktionen mit Definition:
+                    - ausflugMarker: Setzt die Marker auf die Karte, fügt ein Click_Listener hinzu und fügt ein InfoFenster mit Inhalt ein. (Alle Daten aus dem JSON-Response)
+                    - renderHTML: Befüllt den jeweiligen Bereich mit den Karten und die Karteninhalte
+                */
+                function ausflugMarker(props){ 
+                        var marker = new google.maps.Marker({
+                            name:"ausflug"+props.number,
+                            position: new google.maps.LatLng(props.lat,props.lng),
+                            map:map,
+                            icon:"",
+                        });
+                        var infoFenster = new google.maps.InfoWindow({
+                            content:'<div class="pop"><p>'+props.info+'</p></div>'
+                        }); 
+
+                        marker.addListener('click',function(){
+                            infoFenster.open(map,marker);
+                        })
+
+                        if(props.status === "F"){
+                            marker.icon = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+                        }
+                    };
+                function renderHTML(props){
+                    var karte = "<div class='karte'>"+props.info+"</div>";
+                    if(props.status === "F"){
+                        var featured = document.getElementById("f");
+                        featured.insertAdjacentHTML("beforeend",karte);
+                    }else{
+                        var normal = document.getElementById("n");
+                        normal.insertAdjacentHTML("beforeend", karte);
+                    };
+                };
+            };    
+};
+
